9bddbf4d62aaa3b3c23a76d5bb960e75911c7d41
Alekseev Andrey <simpleonus5@mail.ru>
2025-05-05 18:40:31 +0300

    First commit

--- lib/app.py
+++ lib/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi import Request
 from fastapi import Response
 from fastapi import UploadFile
+from fastapi import File
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -13,22 +14,25 @@ from lib.images import open_image
 from lib.models import Reader
 from lib.models import get_model
 
+import numpy as np
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"))
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-def get_index(request: Request) -> Response:
-    return Response(
-        content=f"""
-            <h1>Работает!</h1>
-            <p>теперь загляни в <pre>{__name__.replace(".", "/")}.py</pre></p>
-            <!-- а этот код можно удалить -->
-        """,
-        media_type="text/html",
-    )
-    # return templates.TemplateResponse(request, "index.html")
+def get_form(request: Request) -> Response:
+    return templates.TemplateResponse(request, "upload.html")
+
+
+@app.post("/upload", response_class=HTMLResponse)
+def upload(
+    request: Request,
+    file: UploadFile = File(...),
+    model: Reader = Depends(get_model, use_cache=True)
+):
+    return infer_model(file, request, model)
 
 
 @app.post("/", response_class=HTMLResponse)
@@ -42,7 +46,7 @@ def infer_model(
         image = open_image(file.file)
         draw = PolygonDrawer.from_image(image)
         words = []
-        for coords, word, accuracy in model.readtext(image):
+        for coords, word, accuracy in model.readtext(np.array(image)):
             draw.highlight_word(coords, word)
             cropped_word_image = draw.crop(coords)
             words.append(
@@ -58,5 +62,6 @@ def infer_model(
             words=words,
         )
     except Exception as err:
+        print(err)
         ctx.update(error=str(err))
-    return templates.TemplateResponse(request, "index.html", ctx)
+    return templates.TemplateResponse(request, "info.html", ctx)
